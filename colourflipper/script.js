94f02f1098a717093f2b22ed0659405171694e49
Kampit Ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-12-04 21:22:05 +0530

    flipper

---
+++ colourflipper/script.js
@@ -0,0 +1,18 @@
+function changeColor() {
+    const colorBox = document.getElementById("colorBox");
+    const randomColor = getRandomHexColor();
+    colorBox.style.backgroundColor = randomColor;
+    colorBox.querySelector("p").textContent = randomColor;
+  }
+  
+  function getRandomHexColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+  
+    for (let i = 0; i < 6; i++) {
+      let letterIndex = Math.floor(Math.random() * 16);
+      console.log(letterIndex, letters[letterIndex]);
+      color = color + letters[letterIndex];
+    }
+    return color;
+  }
